7a3c02a689392a42229c32523ce669ce901ee724
Nikolay Derkach <nikolay@toptal.com>
2018-07-16 16:54:26 +0300

    Initial Commit

---
+++ AwesomeFramework/AwesomeFramework.h
@@ -0,0 +1,19 @@
+//
+//  AwesomeFramework.h
+//  AwesomeFramework
+//
+//  Created by Nikolay Derkach on 7/16/18.
+//  Copyright © 2018 Nikolay Derkach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwesomeFramework.
+FOUNDATION_EXPORT double AwesomeFrameworkVersionNumber;
+
+//! Project version string for AwesomeFramework.
+FOUNDATION_EXPORT const unsigned char AwesomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeFramework/PublicHeader.h>
+
+
